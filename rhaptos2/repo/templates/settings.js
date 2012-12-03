aa160abb8f3ba077ce457a71c1b96609ca415ca3
Michael Mulich <michael.mulich@gmail.com>
2012-12-03 11:51:48 -0500

    Global settings variable and removal of the redundant value as well as obscure names.

--- rhaptos2/repo/templates/settings.js
+++ rhaptos2/repo/templates/settings.js
@@ -1,17 +1,13 @@
-// Copyright (c) Rice University 2012
-// This software is subject to 
-// the provisions of the GNU Lesser General
-// Public License Version 2.1 (LGPL).
-// See LICENCE.txt for details.  
-//
+/**
+ * Contains configuration settings that are acquired from the server.
+ *
+ * Author: Paul Brian
+ * Copyright (c) 2012 Rice University
+ *
+ * This software is subject to the provisions of the GNU Lesser General
+ * Public License Version 2.1 (LGPL).  See LICENSE.txt for details.
+ */
 
-
-var FROZONE = {
-
-    e2repoFQDN   : "{{ confd['rhaptos2repo']['www_server_name'] }}", 
-    wwwFQDN      : "{{ confd['rhaptos2repo']['www_server_name'] }}"
-
-
+var SETTINGS = {
+    FQDN: "{{ confd['rhaptos2repo']['www_server_name'] }}",
 }
-
-
