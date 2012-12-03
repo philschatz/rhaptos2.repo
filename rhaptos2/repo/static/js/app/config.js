51c635bd9b65e5d8481791cd7fb00de21037ca17
Michael Mulich <michael.mulich@gmail.com>
2012-12-03 18:41:33 -0500

    Baseline application built on requirejs and backbone startup.

---
+++ rhaptos2/repo/static/js/app/config.js
@@ -0,0 +1,30 @@
+/**
+ * Main entry point for the application configuration and startup.
+ *
+ * Author: Michael Mulich
+ * Copyright (c) 2012 Rice University
+ *
+ * This software is subject to the provisions of the GNU Lesser General
+ * Public License Version 2.1 (LGPL).  See LICENSE.txt for details.
+ */
+
+require.config({
+    deps: ['main'],
+    paths: {
+        lib: '../lib',
+        /* zepto: '', */
+        jquery: '//ajax.googleapis.com/ajax/libs/jquery/1.8.0/jquery',
+        jqueryui: '//ajax.googleapis.com/ajax/libs/jqueryui/1.9.1/jquery-ui.min',
+    },
+    shim: {
+        'lib/backbone': {
+            /* deps: ['jquery', 'lodash'], */
+            deps: ['jquery', 'lib/underscore'],
+            exports: 'Backbone',
+        },
+        'lib/bootstrap': {
+            deps: ['jquery'],
+            exports: 'bootstrap',
+        },
+    },
+})
---
+++ rhaptos2/repo/static/js/app/main.coffee
@@ -0,0 +1,15 @@
+###
+  Contains the main application logic.
+
+  Author: Michael Mulich
+  Copyright (c) 2012 Rice University
+
+  This software is subject to the provisions of the GNU Lesser General
+  Public License Version 2.1 (LGPL).  See LICENSE.txt for details.
+###
+
+require(['jquery', 'lib/backbone', 'lib/underscore', '!lib/bootstrap'], ($, Backbone, _) ->
+
+  console.log('loaded: main')
+
+)
